884010c165d0a064450f5741b0bbde7cfcb66b53
ampats2008 <83134780+ampats2008@users.noreply.github.com>
2021-08-27 13:58:59 -0400

    Add files via upload

--- multiapp.py
+++ multiapp.py
@@ -40,7 +40,7 @@ class MultiApp:
 
     def run(self):
         app = st.sidebar.selectbox(
-            'Page Navigation:',
+            'Select an App to Display:',
             self.apps,
             format_func=lambda app: app['title'])
 
